887e750f8e6e2e6ce13dfd199904016417e0feca
salvarys <123325469+salvarys@users.noreply.github.com>
2023-12-04 10:49:30 -0700

    Added Platform 3D

--- Jumper/Assets/Scripts/Player3DBehavior.cs
+++ Jumper/Assets/Scripts/Player3DBehavior.cs
@@ -8,36 +8,19 @@ public class Player3DBehavior : MonoBehaviour
 
     void Update()
     {
-        // Player movement
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
         transform.Translate(movement);
 
-        // Jumping (optional)
-        // Uncomment the following lines if you want jumping functionality
         /*
         if (Input.GetButtonDown("Jump"))
         {
-            // Add jump logic here
-        }
+            
+        } 
         */
-
-        // Running (optional)
-        // Uncomment the following lines if you want running functionality
-        /*
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveSpeed = 10.0f; // Adjust the running speed
-        }
-        else
-        {
-            moveSpeed = 5.0f; // Set the regular speed
-        }
-        */
-
-        // Add any other controls or interactions as needed
+      
     }
 }
 
